d175ab407296e4a86aa0bb93b94a7282d4b9d4fa
Yuki Shirai <yukishirai1926@gmail.com>
2025-06-08 16:09:35 -0400

    Introduce better test

--- test/robot_joint_test.cpp
+++ test/robot_joint_test.cpp
@@ -4,7 +4,7 @@
 
 using namespace noir;
 
-TEST(RevoluteJointTest, IdentityTransformAtZeroAngle) {
+TEST(JointInerfaceTest, IdentityTransformAtZeroAngle) {
     Eigen::Vector3d axis(0, 0, 1);
     Eigen::Vector3d offset(1, 2, 3);
 
@@ -17,32 +17,28 @@ TEST(RevoluteJointTest, IdentityTransformAtZeroAngle) {
     EXPECT_TRUE(T.isApprox(expected, 1e-9));
 }
 
-TEST(RevoluteJointTest, RotationAboutZAxis) {
+TEST(JointInerfaceTest, RotationAboutZAxis) {
     Eigen::Vector3d axis(0, 0, 1);
     Eigen::Vector3d offset(0, 0, 0);
 
     RevoluteJoint joint(axis, offset);
     Eigen::Matrix4d T = joint.getTransform(M_PI / 2);
+    Eigen::Matrix3d R = T.block<3, 3>(0, 0);
 
     Eigen::Matrix3d expected_R;
     expected_R << 0, -1, 0, 1, 0, 0, 0, 0, 1;
 
-    // EXPECT_PRED2(
-    //     ([](const Eigen::Matrix3d& a, const Eigen::Matrix3d& b) {
-    //         return a.isApprox(b, 1e-9);
-    //     }),
-    //     T.block<3,3>(0,0),
-    //     expected_R
-    // );
+    EXPECT_TRUE(R.isApprox(expected_R, 1e-9));
 }
 
-TEST(JointInterfaceTest, PolymorphicCall) {
+TEST(JointInerfaceTest, PolymorphicCall) {
+    // TODO: test for other different cases over for loop
     std::unique_ptr<Joint> joint =
-        std::make_unique<RevoluteJoint>(Eigen::Vector3d(0, 0, 1), Eigen::Vector3d(1, 0, 0));
-
+        std::make_unique<RevoluteJoint>(Eigen::Vector3d(0, 0, 1), Eigen::Vector3d(1, 2, 3));
+    // this should return derived class
     Eigen::Matrix4d T = joint->getTransform(0.0);
     Eigen::Matrix4d expected = Eigen::Matrix4d::Identity();
-    expected(0, 3) = 1.0;
+    expected.block<3, 1>(0, 3) = Eigen::Vector3d{1, 2, 3};
 
     EXPECT_TRUE(T.isApprox(expected, 1e-9));
 }
