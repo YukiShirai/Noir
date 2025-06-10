5ad3c8d7aee595f4cece6facdea1ef81733394f5
Yuki Shirai <yukishirai1926@gmail.com>
2025-06-09 20:47:27 -0400

    Update joint class

--- test/robot_joint_test.cpp
+++ test/robot_joint_test.cpp
@@ -82,3 +82,60 @@ TEST(JointInterfaceTest, PolymorphicCall) {
         EXPECT_TRUE(T.isApprox(expected, 1e-9));
     }
 }
+
+TEST(JointInterfaceTest, AxisNormalization) {
+    Eigen::Vector3d axis_unnormalized(0, 0, 10);  // not unit length
+    Eigen::Vector3d offset(0, 0, 0);
+
+    RevoluteJoint joint("rev_joint", 0, axis_unnormalized, offset);
+    Eigen::Matrix4d T = joint.getTransform(M_PI);  // rotate 180°
+
+    Eigen::Matrix3d R = T.block<3, 3>(0, 0);
+    Eigen::Matrix3d expected = Eigen::AngleAxisd(M_PI, Eigen::Vector3d(0, 0, 1)).toRotationMatrix();
+
+    EXPECT_TRUE(R.isApprox(expected, 1e-9));
+}
+
+TEST(JointInterfaceTest, PrintOperatorMethod) {
+    RevoluteJoint joint("rev_joint56", 121, Eigen::Vector3d(0, 0, 1), Eigen::Vector3d(1, 2, 3));
+
+    std::ostringstream oss;
+    oss << joint;
+
+    std::string output = oss.str();
+    EXPECT_NE(output.find("rev_joint56"), std::string::npos);
+    EXPECT_NE(output.find("121"), std::string::npos);
+}
+
+TEST(JointInterfaceTest, NameAndIndexAccessors) {
+    RevoluteJoint joint("joint_xyz", 5, Eigen::Vector3d(0, 0, 1), Eigen::Vector3d(1, 2, 3));
+
+    EXPECT_EQ(joint.getName(), "joint_xyz");
+    EXPECT_EQ(joint.getIndex(), 5);
+}
+
+TEST(JointFactoryTest, RevoluteJointTransform) {
+    {
+        Eigen::Vector3d axis(0, 0, 1);
+        Eigen::Vector3d offset(1, 2, 3);
+
+        auto joint = JointFactory::createJoint("revolute", "rev_joint", 0, axis, offset);
+
+        ASSERT_NE(joint, nullptr);
+        Eigen::Matrix4d T = joint->getTransform(0.0);
+
+        Eigen::Matrix4d expected = Eigen::Matrix4d::Identity();
+        expected.block<3, 1>(0, 3) = offset;
+
+        EXPECT_TRUE(T.isApprox(expected, 1e-9));
+    }
+}
+
+TEST(JointFactoryTest, ThrowsOnUnknownJointType) {
+    Eigen::Vector3d axis(0, 0, 1);
+    Eigen::Vector3d offset(0, 0, 0);
+
+    EXPECT_THROW(
+        { auto joint = JointFactory::createJoint("foobar", "invalid_joint", 99, axis, offset); },
+        std::invalid_argument);
+}
